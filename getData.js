a25bfcfd26a3725137e298f3b3845496f3af8a6b
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-07-08 01:21:57 +0700

    default pre-change

--- getData.js
+++ getData.js
@@ -4,41 +4,38 @@ function formatNumber(num) {
 
 function getCountryData(country) {
   let countryInput;
-    if (country !== 'Worldwide'){
-      getCountry = fetch("https://covid19.mathdro.id/api" + `/countries/${country}`);
-    }
-    else{
-      getCountry = fetch("https://covid19.mathdro.id/api")
-    }
-    getCountry
-    .then((response)=> response.json())
-    .then((data)=>{
-      document.getElementById("confirmed-value").innerHTML = formatNumber(data.confirmed.value);
-      document.getElementById("deaths-value").innerHTML = formatNumber(data.deaths.value);
-      document.getElementById("recovered-value").innerHTML = formatNumber(data.recovered.value);
+  if (country !== "Worldwide") {
+    getCountry = fetch(
+      "https://covid19.mathdro.id/api" + `/countries/${country}`
+    );
+  } else {
+    getCountry = fetch("https://covid19.mathdro.id/api");
+  }
+  getCountry
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("confirmed-value").innerHTML = formatNumber(
+        data.confirmed.value
+      );
+      document.getElementById("deaths-value").innerHTML = formatNumber(
+        data.deaths.value
+      );
+      document.getElementById("recovered-value").innerHTML = formatNumber(
+        data.recovered.value
+      );
       document.getElementById("county-name").innerHTML = country;
     })
-    .catch(err=>{
-        window.alert("Error Occured Unable to Fetch Data");
-        console.error(err)
+    .catch((err) => {
+      window.alert("Error Occured Unable to Fetch Data");
+      console.error(err);
     });
 }
 
 function submitFunction(event) {
   event.preventDefault();
-  let countryInput = document.getElementById("country-field").value
-  getCountryData(countryInput)
+  let countryInput = document.getElementById("country-field").value;
+  getCountryData(countryInput);
   return false;
 }
 
-
 getCountryData("Worldwide");
-
-
-
-
-
-
-
-
-
